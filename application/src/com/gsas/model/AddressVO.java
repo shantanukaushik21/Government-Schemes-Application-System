7b17c0a79ff5ac9b2c5f48287f18cc1fd48dd457
shantanukaushik21 <shantanu.kaushik21@gmail.com>
2021-10-01 01:14:00 +0530

    model

---
+++ application/src/com/gsas/model/AddressVO.java
@@ -0,0 +1,99 @@
+package com.gsas.model;
+
+public class AddressVO {
+	
+	private long addressId;
+	private String street;
+	private String city;
+	private String state;
+	private int pincode;
+	public AddressVO() {
+		super();
+	}
+	public AddressVO(long addressId, String street, String city, String state, int pincode) {
+		super();
+		this.addressId = addressId;
+		this.street = street;
+		this.city = city;
+		this.state = state;
+		this.pincode = pincode;
+	}
+	public long getAddressId() {
+		return addressId;
+	}
+	public void setAddressId(long addressId) {
+		this.addressId = addressId;
+	}
+	public String getStreet() {
+		return street;
+	}
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public int getPincode() {
+		return pincode;
+	}
+	public void setPincode(int pincode) {
+		this.pincode = pincode;
+	}
+	@Override
+	public String toString() {
+		return "AddressVO [addressId=" + addressId + ", street=" + street + ", city=" + city + ", state=" + state
+				+ ", pincode=" + pincode + "]";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (addressId ^ (addressId >>> 32));
+		result = prime * result + ((city == null) ? 0 : city.hashCode());
+		result = prime * result + pincode;
+		result = prime * result + ((state == null) ? 0 : state.hashCode());
+		result = prime * result + ((street == null) ? 0 : street.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		AddressVO other = (AddressVO) obj;
+		if (addressId != other.addressId)
+			return false;
+		if (city == null) {
+			if (other.city != null)
+				return false;
+		} else if (!city.equals(other.city))
+			return false;
+		if (pincode != other.pincode)
+			return false;
+		if (state == null) {
+			if (other.state != null)
+				return false;
+		} else if (!state.equals(other.state))
+			return false;
+		if (street == null) {
+			if (other.street != null)
+				return false;
+		} else if (!street.equals(other.street))
+			return false;
+		return true;
+	}
+	
+	
+}
